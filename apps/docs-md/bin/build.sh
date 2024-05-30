314e15fe2f87fe54fa1c94f0e2292e1f4dce238f
Alex Yegupov <yegupov@gmail.com>
2024-05-30 11:16:13 +0300

    single build.sh script

--- apps/docs-md/bin/build.sh
+++ apps/docs-md/bin/build.sh
@@ -1,13 +1,16 @@
 #/bin/bash
 
-# build docsify sidebar from docs/*.md files
 cd $(dirname "$0")/../docs
 
-# 1. Generate docsify sidebar
+# remove old docsify sidebar
+rm -rf ./_sidebar.md
+
+# Remark: normalize markdowns and validate links
+yarn remark -f --use remark-validate-links ./ -o ./
+
+# generate docsify sidebar from *.md files
 echo '<!-- Generated automatically by prepare-docsify.sh -->' > _sidebar.md
 echo '* [Home](README.md)' >> _sidebar.md
-
 ls *.md  |grep -v '^_' |grep -v 'README.md' | xargs -I % -n1 basename % .md |xargs -I % -n1 echo \* [%]\(%.md\) >> _sidebar.md
 
 
-# that's all for now
